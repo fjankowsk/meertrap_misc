a0edbb32d31f427be81ed76d9b369cd07393e673
Fabian Jankowski <fjankowsk@gmail.com>
2019-01-31 13:13:03 +0000

    Plot beam packing and time it.

--- beam_packer.py
+++ beam_packer.py
@@ -10,6 +10,7 @@ import logging
 import os.path
 import matplotlib.pyplot as plt
 import sys
+from timeit import default_timer as timer
 
 logger = logging.getLogger()
 
@@ -24,7 +25,7 @@ def load_data(filename):
     dtype = [("x","float"), ("y","float")]
     temp = np.genfromtxt(filename, delimiter="\t", dtype=dtype)
 
-    dtype = [("nr","int"), ("x","float"), ("y","float"), ("picked","bool")]
+    dtype = [("nr","int"), ("x","float"), ("y","float"), ("group","int")]
     data = np.zeros(len(temp), dtype=dtype)
 
     data["nr"] = np.arange(len(data))
@@ -54,6 +55,54 @@ def plot_beam_centres(t_data):
 
     fig.tight_layout()
 
+    fig.savefig("beam_centres.pdf", bbox_inches="tight")
+    fig.savefig("beam_centres.png", bbox_inches="tight",
+                dpi=200)
+
+
+def plot_beam_mapping(t_data):
+    """
+    Plot the beam mapping.
+    """
+
+    data = np.copy(t_data)
+
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    i = 0
+
+    for group in np.unique(data["group"]):
+        color = colors[i % len(colors)]
+
+        indiv = data[data["group"] == group]
+
+        ax.scatter(indiv["x"], indiv["y"],
+                marker="x",
+                color=color,
+                label=group)
+        
+        i += 1
+    
+    leg = ax.legend(loc="upper center", frameon=False, ncol=11, fontsize=9,
+                    bbox_to_anchor=(0.5, -0.13),
+                    columnspacing=0.2,
+                    labelspacing=0.1)
+    leg.set_zorder(10)
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.grid(True)
+
+    fig.tight_layout()
+
+    fig.savefig("beam_mapping.pdf", bbox_inches="tight")
+    fig.savefig("beam_mapping.png", bbox_inches="tight",
+                dpi=200)
+
 
 def get_beam_mapping(t_data):
     """
@@ -77,7 +126,7 @@ def get_beam_mapping(t_data):
     work = np.copy(data)
 
     while len(work) != 0:
-        print("Length: {0}".format(len(work)))
+        logger.debug("Length: {0}".format(len(work)))
 
         dist = np.sqrt((work["x"] - work["x"][0])**2 + (work["y"] - work["y"][0])**2)
         tot = np.zeros(len(work), dtype=dtype)
@@ -91,11 +140,16 @@ def get_beam_mapping(t_data):
 
         # pick the closest six beams
         picked = tot[0:bunch]
-        print("Group: {0}, beams: {1}".format(group, picked["nr"]))
+        logger.debug("Group: {0}, beams: {1}".format(group, picked["nr"]))
 
-        mask = np.isin(work["nr"], picked["nr"], invert=True)
-        work = work[mask]
+        mask_work = np.isin(work["nr"], picked["nr"], invert=True)
+        mask_data = np.isin(data["nr"], picked["nr"])
+        work = work[mask_work]
+        data["group"][mask_data] = group
+
         group += 1
+    
+    return data
 
 
 #
@@ -107,7 +161,17 @@ def main():
     data = load_data(infile)
 
     plot_beam_centres(data)
-    get_beam_mapping(data)
+
+    start = timer()
+    mapped = get_beam_mapping(data)
+    end = timer()
+
+    print("Elapsed time: {0:.2f} ms".format(1000*(end - start)))
+
+    for item in mapped:
+        logger.info("Beam: {0}, group: {1}".format(item["nr"], item["group"]))
+
+    plot_beam_mapping(mapped)
 
     plt.show()
 
